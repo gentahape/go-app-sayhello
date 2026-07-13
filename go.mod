fe8ba8fd2af36310cd81d8c0477c504e74bdcd13
Genta <gentahp12@gmail.com>
2026-07-13 14:23:23 +0700

    update go.mod and go.sum to require github.com/gentahape/go-mod-sayhello/v2 v2.0.0; modify main.go to use updated module and pass name to SayHello function

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module go-app-sayhello
 
 go 1.26.3
 
-require github.com/gentahape/go-mod-sayhello v1.5.0
+require github.com/gentahape/go-mod-sayhello/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	go_say_hello "github.com/gentahape/go-mod-sayhello"
+	go_say_hello "github.com/gentahape/go-mod-sayhello/v2"
 )
 
 func main() {
-	fmt.Println(go_say_hello.SayHello())
+	fmt.Println(go_say_hello.SayHello("John"))
 }

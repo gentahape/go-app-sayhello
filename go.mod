9dc02fa6a53b5957e6bcb6b6ca4df66a9f331f45
Genta <gentahp12@gmail.com>
2026-07-13 14:14:45 +0700

    add main application and update go.mod with required module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-app-sayhello
 
 go 1.26.3
+
+require github.com/gentahape/go-mod-sayhello v1.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/gentahape/go-mod-sayhello"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
